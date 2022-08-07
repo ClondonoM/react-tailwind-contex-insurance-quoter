cb121440c8ac16529bb60feab2516cbf73d4f4fd
Carlos Londono <c.londono.m@gmail.com>
2022-08-07 10:01:42 -0500

    quoter function added

--- src/context/QuoterProvider.jsx
+++ src/context/QuoterProvider.jsx
@@ -1,4 +1,10 @@
 import { useState, createContext } from 'react';
+import {
+  getYearDifference,
+  brandFactor,
+  planFactor,
+  moneyFormat,
+} from '../helpers';
 
 const QuoterContext = createContext();
 
@@ -9,14 +15,27 @@ const QuoterProvider = ({ children }) => {
     plan: '',
   });
   const [error, setError] = useState('');
+  const [result, setResult] = useState(0);
   const handleChangeData = (e) => {
     setData({
       ...data,
       [e.target.name]: e.target.value,
     });
   };
+
+  const quoteInsurance = () => {
+    let result = 2000;
+    const yearDifference = getYearDifference(data.year);
+    result -= (yearDifference * 3 * result) / 100;
+    result = brandFactor(data.brand) * result;
+    result = moneyFormat(planFactor(data.plan) * result);
+    setResult(result);
+  };
+
   return (
-    <QuoterContext.Provider value={{ data, handleChangeData, error, setError }}>
+    <QuoterContext.Provider
+      value={{ data, handleChangeData, error, setError, quoteInsurance }}
+    >
       {children}
     </QuoterContext.Provider>
   );
